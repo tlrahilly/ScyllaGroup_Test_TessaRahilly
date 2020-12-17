4809260debcfaf1c0c63ca77f8510c88330c20ab
tlrahilly <tlrahilly@aol.com>
2020-12-16 20:10:41 -0500

    working on pagination

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Get the button
-var myBtn = document.getElementById("myBtn"); 
+let myBtn = document.getElementById("myBtn"); 
 
 //When the user scrolls down 20px from the top of the document, show the button
 window.addEventListener("scroll", function() {
@@ -16,4 +16,41 @@ window.addEventListener("scroll", function() {
         document.documentElement.scrollTop = 0;
       })
 });
+
+
+// Pagination for blog page
+let articleList = document.querySelector('#article-list').children;
+let prevBtn = document.querySelector('.page-prev');
+let nextBtn = document.querySelector('.page-next');
+let pageNum = document.querySelector('.page-number');
+let currentPage = 1;
+let perPage = 6;
+const pagination = Math.ceil(articleList.length/perPage);
+
+function showItems(){
+  for(let i = 0; i < articleList.length; i++){
+    articleList[i].classList.add('hide');
+    
+    if (i>=(currentPage*perPage)-perPage && i<currentPage*perPage){
+      articleList[i].classList.toggle('hide');
+    }
+  }
+  pageNum.textContent =`${currentPage} of ${pagination}`;
+}
+
+window.onload=showItems();
+
+nextBtn.addEventListener('click', function () {
+  currentPage++;
+  showItems();
+  return false;
+})
+
+prevBtn.addEventListener('click', function(){
+  currentPage--;
+  showItems();
+  return false;
+})
+
+
 
